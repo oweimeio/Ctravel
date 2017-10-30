074ed8b3f770ea488f188c7b2d61da97aad8dcd5
Huwei <oweimeio@qq.com>
2017-10-30 22:58:34 +0800

    Add :

---
+++ Ctravel/_scene/_client/favorite/favDetail/BuyProViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuyProViewController.h
+//  Ctravel
+//
+//  Created by apple on 2017/10/22.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuyProViewController : UIViewController
+
+@end
